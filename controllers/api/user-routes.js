04bf9cf88107bd2dad4767250b4d72a758b8d17b
nicolewallace09 <nicole.elisaw@gmail.com>
2020-06-12 15:58:16 -0700

    created api routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -52,7 +52,7 @@ router.get('/:id', (req, res) => {
 });
 
 // CREATE new user 
-router.post('/', withAuth, (req, res) => {
+router.post('/', (req, res) => {
     User.create({
         username: req.body.username,
         email: req.body.email,
@@ -100,6 +100,64 @@ router.post('/login', (req, res) => {
         });
     });
 });
+
+// LOG OUT 
+router.post('/logout', withAuth, (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
+
+// UPDATE a user 
+router.put('/:id', withAuth, (req, res) => {
+    User.update(req.body, {
+        individualHooks: true,
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData[0]) {
+            res.status(404).json({ message: 'No user found with this id'});
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err); 
+        res.status(500).json(err);
+    });
+});
+
+// DELETE a user
+router.delete('/:id', withAuth, (req, res) => {
+    User.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id'});
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+module.exports = router;
+
+
+
+
 
 
 
